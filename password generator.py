b525cc24bf83885dd703c85de633dd934a8b3bab
YingCG <68981715+YingCG@users.noreply.github.com>
2021-05-19 17:30:12 +1200

    Update password generator.py

--- password generator.py
+++ password generator.py
@@ -1,25 +1,26 @@
 #In order to access the Python library, we need to import the package in our Python script.
+# Tips: https://medium.com/analytics-vidhya/create-a-random-password-generator-using-python-2fea485e9da9
 
 import random
 import string
 
 #test with greeting
 print('hello, Welcome to Password generator!')
+length = int(input('\n Enter the length of your password'))
 
-#Next, let’s ask the user for the length of the password.
-length = int(input('\nEnter the length of password: '))
-
-# to define the data
-lower = string.ascii_lowercase
+#define variable
+letter = string.ascii_letters
 upper = string.ascii_uppercase
+lower = string.ascii_lowercase
 num = string.digits
 symbols = string.punctuation
 
-#combine and store the data
-all = lower + upper + num + symbols
-
-#generate the password
-temp = random.sample(all,length)
+all = upper + lower + num + symbols
+temp = random.sample(all, length)
 password = "".join(temp)
+print (password)
 
-#https://medium.com/analytics-vidhya/create-a-random-password-generator-using-python-2fea485e9da9
+#or shorter way to write below
+all = string.ascii_letters + string.digits + string.punctuation
+password = "".join(random.sample(all, length))
+print (password)
